efce2589d9464cfc08a7721f9c09479be3c31d72
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-09-19 11:12:08 +0800

    part 1 works

--- lab1/ex1.py
+++ lab1/ex1.py
@@ -9,32 +9,38 @@ import argparse
 import string
 
 def enc(letter, key):
-    pass
+    return chr((ord(letter)+key)%256)
 
 def dec(letter, key):
-    pass
+    return enc(letter, -key)
 
-def doStuff(filein, fileout, mode_e_d):
+def doStuff(filein, fileout, mode_e_d, key):
     # open file handles to both files
     fin = open(filein, mode="r", encoding="utf-8", newline="\n")  # read mode
     fin_b = open(filein, mode="rb")  # binary read mode
     fout = open(fileout, mode="w", encoding="utf-8", newline="\n")  # write mode
     fout_b = open(fileout, mode="wb")  # binary write mode
     c = fin.read()  # read in file into c as a str
+
+    #loop thru to get each character for enc and dec 
+    output_to_file = ""
+
+    if (mode_e_d == "e"):
+        for character in c:
+            output_to_file += enc(character, key)
+    
     # and write to fileout
+    if(mode_e_d == "d"):
+        for character in c:
+            output_to_file += dec(character, key)
+    
+    fout.write(output_to_file)
 
     # close all file streams
     fin.close()
     fin_b.close()
     fout.close()
     fout_b.close()
-
-    # PROTIP: pythonic way
-    with open(filein, mode="r", encoding="utf-8", newline="\n") as fin:
-        text = fin.read()
-        # do stuff
-
-        # file will be closed automatically when interpreter reaches end of the block
 
 
 # our main function
@@ -53,14 +59,14 @@ if __name__ == "__main__":
     filein = args.filein
     fileout = args.fileout
     #check the keys
-    if((1<=args.k<=len(string.printable)-1) == False):
+    if((1<=args.key<=len(string.printable)-1) == False):
         raise ValueError('The value of the key must be between 1 and that of string.printable')
     
     #check mode arguments
-    if(args.mode == "e" or args.mode == "E" or args.mode=="d" or args.mode=="D"):
-        doStuff(filein, fileout, args.mode)
+    if(args.mode.lower() == "e" or args.mode.lower()=="d"):
+        doStuff(filein, fileout, args.mode.lower(), args.key)
     else:
-        raise ValueError('The mode has to be only \"d" or \"e" case insenstive')
+        raise SyntaxError('The mode has to be only \"d" or \"e" case insensitive')
 
     
     
