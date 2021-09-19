e54332390956861166cf14ea482e8ece170ecfb1
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-09-19 15:44:27 +0800

    part 2 works

--- lab1/ex2.py
+++ lab1/ex2.py
@@ -6,16 +6,32 @@
 # Import libraries
 import sys
 import argparse
+import string
 
+def enc(byte, key):
+    return ((byte + key)%256)
 
-def doStuff(filein, fileout):
+def dec(byte, key):
+    return enc(byte, -key)
+
+def doStuff(filein, fileout, key, mode):
     # open file handles to both files
     fin = open(filein, mode="r", encoding="utf-8", newline="\n")  # read mode
     fin_b = open(filein, mode="rb")  # binary read mode
     fout = open(fileout, mode="w", encoding="utf-8", newline="\n")  # write mode
     fout_b = open(fileout, mode="wb")  # binary write mode
-    c = fin.read()  # read in file into c as a str
+    c = fin_b.read()  # read in file into c as a str
     # and write to fileout
+    output = bytearray()
+    input_to_byte = bytearray(c)
+    if mode == "e":
+        for b in input_to_byte:
+            output.append(enc(b, key))
+    if mode == "d":
+        for b in input_to_byte:
+            output.append(dec(b, key))
+    #write the bytearray to the enc file 
+    fout_b.write(output)
 
     # close all file streams
     fin.close()
@@ -23,12 +39,6 @@ def doStuff(filein, fileout):
     fout.close()
     fout_b.close()
 
-    # PROTIP: pythonic way
-    with open(filein, mode="r", encoding="utf-8", newline="\n") as fin:
-        text = fin.read()
-        # do stuff
-
-        # file will be closed automatically when interpreter reaches end of the block
 
 
 # our main function
@@ -37,12 +47,30 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-i", dest="filein", help="input file")
     parser.add_argument("-o", dest="fileout", help="output file")
-
+    parser.add_argument("-k", dest="key", help="key", type=int)
+    parser.add_argument("-m", dest="mode", help="encryption or decryption")
     # parse our arguments
     args = parser.parse_args()
+    mode = args.mode
+    key= args.key
     filein = args.filein
     fileout = args.fileout
 
-    doStuff(filein, fileout)
+    #check key 
+    if((0 <= key <=255) == False):
+        raise ValueError('The value of the key must be between 1 and that of 255')
+    
+    #check mode
+    if((mode.lower()== "e" or mode.lower()=="d")):
+        pass
+    else:
+        raise SyntaxError('The mode has to be only \"d" or \"e" case insensitive')
+    #TODO input as the size of bytes 
+    #TODO how many characters each byte shifted
+    #TODO consider ASCII 256 
+    #TODO write an enc and dec function + and - respectively
+
+
+    doStuff(filein, fileout, key, mode.lower())
 
     # all done
