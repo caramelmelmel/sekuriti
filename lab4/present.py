e47a8d86917ffb3da25dbb761fec462663f65d8b
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-10-10 17:40:53 +0800

    present has suceeded ripsss :'D

--- lab4/present.py
+++ lab4/present.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # Completed by:
 # Leong Yun Qin Melody 1004489
+# Adapted the codes from: https://github.com/xSAVIKx/PRESENT-cipher/blob/master/present/pyPresent.py
 
 # Present skeleton file for 50.042 FCS
 
@@ -12,11 +13,14 @@ FULLROUND = 31
 sbox = [0xC, 0x5, 0x6, 0xB, 0x9, 0x0, 0xA, 0xD,
         0x3, 0xE, 0xF, 0x8, 0x4, 0x7, 0x1, 0x2]
 
+sbox_inv = [sbox.index(x) for x in range(16)]
 # PLayer
 pmt = [0, 16, 32, 48, 1, 17, 33, 49, 2, 18, 34, 50, 3, 19, 35, 51,
        4, 20, 36, 52, 5, 21, 37, 53, 6, 22, 38, 54, 7, 23, 39, 55,
        8, 24, 40, 56, 9, 25, 41, 57, 10, 26, 42, 58, 11, 27, 43, 59,
        12, 28, 44, 60, 13, 29, 45, 61, 14, 30, 46, 62, 15, 31, 47, 63]
+
+pmt_inv = [pmt.index(x) for x in range(64)]
 
 # Rotate left: 0b1001 --> 0b0011
 
@@ -47,6 +51,7 @@ def genRoundKeys(key):
         key_dict[i] = key >> 16
         # 1. Shift
         # rawKey[19:len(rawKey)]+rawKey[0:19]
+        #shift by 61
         key = ((key & (2 ** 19 - 1)) << 61) + (key >> 19)
         # 2. SBox
         # rawKey[76:80] = S(rawKey[76:80])
@@ -58,18 +63,40 @@ def genRoundKeys(key):
 
 
 def addRoundKey(state, Ki):
-    pass
+    #xor operation
+    return state ^ Ki
+
+# this layer deals with the encryption
+def sBoxLayer(state,mode):
+    if mode == 'e':
+        new_state = 0
+        for i in range(16):
+            new_state += sbox[( state >> (i * 4)) & 0xF] << (i * 4)
+    
+    if mode == 'd':
+        new_state = 0
+        for i in range(16):
+            new_state += sbox_inv[( state >> (i * 4)) & 0xF] << (i * 4)
+    return new_state
 
 
-def sBoxLayer(state):
-    pass
+def pLayer(state,mode):
+    if mode == 'e':
+        output = 0
+        for i in range(64):
+            output += ((state >> i) & 0x01) << pmt[i]
 
-
-def pLayer(state):
-    pass
+    if mode == 'd':
+        output = 0
+        for i in range(64):
+            output += ((state >> i) & 0x01) << pmt_inv[i]
+    return output
 
 
 def present_round(state, roundKey):
+    state = addRoundKey(state, roundKey)
+    state = sBoxLayer(state, 'e')
+    state = pLayer(state,'e')
     return state
 
 
@@ -102,7 +129,7 @@ if __name__ == "__main__":
     keysTest = {0: 32, 1: 0, 2: 13835058055282163712, 3: 5764633911313301505, 4: 6917540022807691265, 5: 12682149744835821666, 6: 10376317730742599722, 7: 442003720503347, 8: 11529390968771969115, 9: 14988212656689645132, 10: 3459180129660437124, 11: 16147979721148203861, 12: 17296668118696855021, 13: 9227134571072480414, 14: 4618353464114686070, 15: 8183717834812044671, 16: 1198465691292819143, 17: 2366045755749583272, 18: 13941741584329639728, 19: 14494474964360714113, 20: 7646225019617799193, 21: 13645358504996018922, 22: 554074333738726254, 23: 4786096007684651070, 24: 4741631033305121237, 25: 17717416268623621775, 26: 3100551030501750445, 27: 9708113044954383277, 28: 10149619148849421687, 29: 2165863751534438555, 30: 15021127369453955789, 31: 10061738721142127305, 32: 7902464346767349504}
     for k in keysTest.keys():
         assert keysTest[k] == keys[k]
-    print("success in asserting the key")
+    #print("success in asserting the key")
 
     # Testvectors for single rounds without keyscheduling
     plain1 = 0x0000000000000000
@@ -119,6 +146,7 @@ if __name__ == "__main__":
     round33 = 0xcc3fcc3f33c00000
     assert round3 == round33
 
+    print('present rounds have suceeded')
     # invert single rounds
     plain11 = present_inv_round(round1, key1)
     assert plain1 == plain11
