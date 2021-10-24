fe2de5d6205b81127ae0a0006650b1e89862dab7
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-10-24 22:07:43 +0800

    square multiply works

--- lab6/primes.py
+++ lab6/primes.py
@@ -3,7 +3,21 @@
 
 import random
 def square_multiply(a,x,n):
-    pass
+    y = 1
+
+    #number of bits in x
+    num_bits = x.bit_length()
+
+    #convert the integer x into a lit of bits
+    x_in_bits = list("{0:b}".format(x))
+
+    for i in x_in_bits:
+        y = (y ** 2) % n 
+        if i == '1':
+            y = y* a %n
+    #print(y)
+    return y 
+
 
 def miller_rabin(n, a):
     pass
@@ -12,6 +26,7 @@ def gen_prime_nbits(n):
     pass
 
 if __name__=="__main__":
+    print(square_multiply(16,12,1))
     print('Is 561 a prime?')
     print(miller_rabin(561,2))
     print('Is 27 a prime?')
