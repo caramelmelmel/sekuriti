50be43f0bff4e418c0c405229ddadfc3d31929d9
caramelmelmel <melody_leong@mymail.sutd.edu.sg>
2021-11-13 22:42:51 +0800

    removed the previous python file

--- lab7/ex5.py
+++
@@ -1,8 +0,0 @@
-# OAEP for enc
-#PSS for digital signature
-from Crypto.PublicKey.RSA import generate
-
-def generate_RSA(bits=1024):
-    #generate in pem format
-    print(generate(bits))
-print(generate_RSA())
